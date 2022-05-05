b076f729bcab1b96ba8f99e8e08c0727b1e92e00
alessflame <francescoalessi.code@libero.it>
2022-05-05 17:25:51 +0200

    update component

--- src/views/components/Footer.js
+++ src/views/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className='footer'>
          <span>
-              Per visualizzare altri progetti vai al mio account <a href='/'>GitHub</a>
+              Per visualizzare altri progetti vai al mio account <a style={{color:"black"}} href='https://github.com/alessflame'>GitHub</a>
          </span>
     </div>
   )
