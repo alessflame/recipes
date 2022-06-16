f94a158689fbf6bab0b0de8e60cd336f3d531286
alessflame <francescoalessi.code@libero.it>
2022-06-16 18:17:15 +0200

    update app

--- src/views/components/Slide.js
+++ src/views/components/Slide.js
@@ -6,7 +6,10 @@ function Slide({image, text, title, visible}) {
   return (
     <div className={visible===true ? stile.slideSelected : stile.slide}>
      <img className={stile.image} src={image} alt="slider"/>
+
+     <section className={stile.sectionText}>
     <TextCard title={title} text={text}/>
+     </section>
    </div>
   )
 }
